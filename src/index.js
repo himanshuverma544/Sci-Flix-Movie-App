1f5f3e60098f27421e667e42983776b656a6ebd2
Himanshu Verma <himanshuverma544@gmail.com>
2023-06-20 20:15:45 +0530

    update: implemented Context Provider

--- src/index.js
+++ src/index.js
@@ -6,11 +6,15 @@ import App from './App';
 import { Provider } from "react-redux";
 import store from "./redux/store";
 
+import ContextProvider from "./context-API/ContextProvider";
+
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <Provider store={store}>
-    <App />
-  </Provider>
+  <ContextProvider>
+    <Provider store={store}>
+      <App/>
+    </Provider>
+  </ContextProvider>
 );
 
