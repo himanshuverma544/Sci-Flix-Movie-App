65ecc8bf26e71739ff39e11d159a65ce5a084305
Himanshu Verma <himanshuverma544@gmail.com>
2024-02-01 16:04:11 +0530

    rn: renamed the imports as per the slices folder

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,8 +1,8 @@
 import { configureStore } from "@reduxjs/toolkit";
 
-import usersMoviesReducer from "./usersMovies";
-import usersReducer from "./users";
-import moviesCommentsReducer from "./moviesComments";
+import usersMoviesReducer from "./slices/usersMovies";
+import usersReducer from "./slices/users";
+import moviesCommentsReducer from "./slices/moviesComments";
 
 const store = configureStore({
   reducer: {
